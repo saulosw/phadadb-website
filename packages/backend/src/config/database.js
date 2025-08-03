c019e1e370035bed955bcfb9b08a17ce87c2d882
Saulo Pereira Jesus <saulopereiradejesus@gmail.com>
2025-08-03 16:15:28 -0300

    feat(database): implement PostgreSQL connection pool

---
+++ packages/backend/src/config/database.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg');
+const path = require('path');
+const dotenv = require('dotenv')
+
+dotenv.config({path: path.resolve(__dirname, '..', '.env')});
+
+const pool = new Pool({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    port: process.env.DATABASE_PORT,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000
+});
+
+module.exports = {
+    query: (text, params) => pool.query(text, params)
+};
---
+++ packages/backend/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config({path: path.resolve(__dirname, '..', '.env')});
+
+const apiPort = process.env.API_PORT;
+
+app.listen(apiPort, () => {
+    console.log(`API is running at http://localhost:${apiPort}`)
+});
